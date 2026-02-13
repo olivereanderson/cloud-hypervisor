59466380eeec0a5a841746708490b64eeaf50eaa
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2026-02-13 18:49:21 +0100

    arch: Start adding list of non-architectural MSRs
    Added list of non-architectural MSRS
    
    Signed-off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- arch/src/x86_64/msr_definitions/intel/mod.rs
+++ arch/src/x86_64/msr_definitions/intel/mod.rs
@@ -4,6 +4,7 @@
 //
 mod architectural_msrs;
 mod msr_based_features;
+mod non_architectural_msrs;
 
 pub use msr_based_features::INTEL_MSR_FEATURE_DEFINITIONS;
 pub(in crate::x86_64) use msr_based_features::check_feature_msr_compatibility;
