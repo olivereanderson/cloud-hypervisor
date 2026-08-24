4920b0a000a5102905da7577a13fdf262f54ae43
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2026-08-24 12:35:28 +0200

    arch: Add compatibility check for CPUID 0xd.0x0:EAX
    CPUID 0xd.0x0:EAX corresponds to the valid bit fields of the lower 32
    bits of the XFEATURE_ENABLED_MASK register. In other words it describes
    which XSTATE components are supported by the CPU.
    
    The live migration destination must necessarily support all XSTATE
    components that the migration source supports.
    
    While it could be the case that KVM will not report CPU features
    (in say leaf 0x7 or 0x1) whose corresponding state components are not
    supported by the host, this is not documented behavior to the best of
    our knowledge. Since adding this extra check is very simple we think
    it is reasonable to err on the side of caution here.
    
    Note that certain bits within this register value have different
    meaning on AMD and Intel CPUs, but we do not need to worry about that
    since several other compatibility checks should fail in the case of
    live migration between Intel and AMD CPUs.
    
    Signed-off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- arch/src/x86_64/mod.rs
+++ arch/src/x86_64/mod.rs
@@ -474,6 +474,12 @@ impl CpuidFeatureEntry {
                 compatible_check: CpuidCompatibleCheck::BitwiseSubset,
             },
             CpuidFeatureEntry {
+                function: 0xd,
+                index: 0,
+                feature_reg: CpuidReg::EAX,
+                compatible_check: CpuidCompatibleCheck::BitwiseSubset,
+            },
+            CpuidFeatureEntry {
                 function: 0x1d,
                 index: 0,
                 feature_reg: CpuidReg::EAX,
